6de9aa52ca705b45b388a136602a20272c074172
ouyang <1262014533@qq.com>
2019-07-24 11:16:56 +0800

    .

--- src/main/java/com/ouyang/redislock/schedual/WebSocketSchedual.java
+++ src/main/java/com/ouyang/redislock/schedual/WebSocketSchedual.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
