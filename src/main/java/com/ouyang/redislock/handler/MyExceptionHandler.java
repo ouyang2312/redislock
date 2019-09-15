cdeaf6fb9d9c06abd10af57becccfdb9a3bb1382
ouyang <1262014533@qq.com>
2019-09-15 15:14:52 +0800

    ...

--- src/main/java/com/ouyang/redislock/handler/MyExceptionHandler.java
+++ src/main/java/com/ouyang/redislock/handler/MyExceptionHandler.java
@@ -2,8 +2,10 @@ package com.ouyang.redislock.handler;
 
 import com.ouyang.redislock.entity.ResponseBody;
 import com.ouyang.redislock.exception.MyException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,10 +15,11 @@ import javax.servlet.http.HttpServletResponse;
  * @description
  * @date 2019/7/18
  */
+@org.springframework.web.bind.annotation.ResponseBody
 @ControllerAdvice
 public class MyExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ResponseBody
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler({MyException.class})
     public ResponseBody handleArithmeticException(Exception ex, HttpServletRequest request, HttpServletResponse response) throws Exception {
         String message = ex.getMessage();
